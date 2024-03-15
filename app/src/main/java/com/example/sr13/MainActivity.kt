40c7acff65630a5549ec28e517fadf621a74816d
pawelglaba <pawel.glaba@pwr.edu.pl>
2024-03-15 11:44:44 +0100

    New version of simple Android App

--- app/src/main/java/com/example/sr13/MainActivity.kt
+++ app/src/main/java/com/example/sr13/MainActivity.kt
@@ -9,53 +9,48 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity() {
 
-    private var userInput: EditText? = null
-    private var button: Button? = null
-    private var textView: TextView? = null
-
-    private var nextViewButton: Button? = null
-
-
+    private lateinit var userInput: EditText
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var nextViewButton: Button
     private var numTimesClicked = 0
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicjalizacja elementów interfejsu użytkownika
         userInput = findViewById(R.id.inputName)
         button = findViewById(R.id.button)
         textView = findViewById(R.id.printText)
 
-        textView?.text = ""
-        textView?.movementMethod = ScrollingMovementMethod()
+        textView.text = ""
+        textView.movementMethod = ScrollingMovementMethod()
 
         nextViewButton = findViewById(R.id.nextView)
 
+        // Obsługa kliknięcia przycisku "Next View"
+        nextViewButton.setOnClickListener {
+            openActivity2()
+        }
 
-        nextViewButton?.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                openActivity2()
+        // Obsługa kliknięcia przycisku
+        button.setOnClickListener {
+            numTimesClicked++
+            textView.append("\nThe button got tapped by ${userInput.text} $numTimesClicked time")
+            if(numTimesClicked != 1){
+                textView.append("s")
             }
-        })
-
-
-        button?.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                numTimesClicked += 1
-                textView?.append("\n The button got tapped by ${userInput?.text} $numTimesClicked time")
-                if(numTimesClicked !=1){
-                    textView?.append("s")
-                }
-            }
-        })
+        }
     }
 
+    // Metoda do otwierania drugiej aktywności
     private fun openActivity2(){
         val intent = Intent(this, secondActivity::class.java)
+        intent.putExtra("userName", userInput.text.toString())
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/sr13/secondActivity.kt
+++ app/src/main/java/com/example/sr13/secondActivity.kt
@@ -3,31 +3,36 @@ package com.example.sr13
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class secondActivity : AppCompatActivity() {
 
-    private var backButton: Button? = null
-
-
+    private lateinit var backButton: Button
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        backButton = findViewById(R.id.backButton)
-        backButton?.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-              backActivity();
-            }
-        })
+        // Odbieranie danych przekazanych z poprzedniej aktywności
+        val userName = intent.getStringExtra("userName")
 
+        // Inicjalizacja elementów interfejsu użytkownika
+        textView = findViewById(R.id.printName)
+        textView.text = userName
+
+        backButton = findViewById(R.id.backButton)
+
+        // Obsługa kliknięcia przycisku "Back"
+        backButton.setOnClickListener {
+            backActivity()
+        }
     }
 
+    // Metoda do powrotu do poprzedniej aktywności
     private fun backActivity(){
-        val intent= Intent(this, MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
-
 }
