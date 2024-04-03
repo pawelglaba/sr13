d51f328349ba043bf85ca44a96c788e60d3877f3
pawelglaba <pawel.glaba@pwr.edu.pl>
2024-04-03 22:51:46 +0200

    lecture

--- app/src/main/java/com/example/sr13/MainActivity.kt
+++ app/src/main/java/com/example/sr13/MainActivity.kt
@@ -1,56 +1,67 @@
+
 package com.example.sr13
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var userInput: EditText
-    private lateinit var button: Button
-    private lateinit var textView: TextView
-    private lateinit var nextViewButton: Button
+    // Deklaracja obiektow
+    private var userInput: EditText? = null
+    private var pushButton: Button? = null
+    private var textView: TextView? = null
+    private var nextViewButton: Button? = null
     private var numTimesClicked = 0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        // Ustawienie widoku interfejsu użytkownika na activity_main.xml
+        setContentView(R.layout.activity_main)
         // Inicjalizacja elementów interfejsu użytkownika
         userInput = findViewById(R.id.inputName)
-        button = findViewById(R.id.button)
+        pushButton = findViewById(R.id.pushButton)
         textView = findViewById(R.id.printText)
 
-        textView.text = ""
-        textView.movementMethod = ScrollingMovementMethod()
+        textView?.text = resources.getString(R.string.helloWorld)
+        // Ustawienie przewijania w TextView
+        textView?.movementMethod = ScrollingMovementMethod()
 
+        // Inicjalizacja przycisku przejścia do kolejnej aktywności
         nextViewButton = findViewById(R.id.nextView)
 
-        // Obsługa kliknięcia przycisku "Next View"
-        nextViewButton.setOnClickListener {
-            openActivity2()
-        }
-
-        // Obsługa kliknięcia przycisku
-        button.setOnClickListener {
-            numTimesClicked++
-            textView.append("\nThe button got tapped by ${userInput.text} $numTimesClicked time")
-            if(numTimesClicked != 1){
-                textView.append("s")
+        // Ustawienie nasłuchiwacza zdarzeń dla przycisku przejścia do kolejnej aktywności
+        nextViewButton?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openSecondActivity()
             }
-        }
+        })
+
+        // Ustawienie nasłuchiwacza zdarzeń dla przycisku
+        pushButton?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                numTimesClicked += 1
+                textView?.append("\n The button got tapped by ${userInput?.text} $numTimesClicked time")
+                if(numTimesClicked !=1){
+                    textView?.append("s")
+                }
+            }
+        })
     }
 
-    // Metoda do otwierania drugiej aktywności
-    private fun openActivity2(){
-        val intent = Intent(this, secondActivity::class.java)
-        intent.putExtra("userName", userInput.text.toString())
+    // Metoda otwierająca drugą aktywność
+    private fun openSecondActivity(){
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("name",userInput?.text.toString())
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/sr13/SecondActivity.kt
+++ app/src/main/java/com/example/sr13/SecondActivity.kt
@@ -1,26 +1,28 @@
 package com.example.sr13
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-class secondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     private lateinit var backButton: Button
     private lateinit var textView: TextView
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         // Odbieranie danych przekazanych z poprzedniej aktywności
-        val userName = intent.getStringExtra("userName")
+        val userName = intent.getStringExtra("name")
 
         // Inicjalizacja elementów interfejsu użytkownika
         textView = findViewById(R.id.printName)
-        textView.text = userName
+        textView.text = "Hello ,$userName!"
 
         backButton = findViewById(R.id.backButton)
 
